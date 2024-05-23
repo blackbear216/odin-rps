71653e92e7fb4d7b4de28ee8a3f72425bc0b1d36
blackbear <98885273+blackbear216@users.noreply.github.com>
2024-05-23 14:44:06 +0100

    Full game implemented, plays 5 rounds

--- script.js
+++ script.js
@@ -15,50 +15,67 @@ function getHumanChoice() {
     return humanChoice.toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    const drawString = `Draw! ${humanChoice} matches ${computerChoice}`;
-    const winString = `You win! ${humanChoice} beats ${computerChoice}`;
-    const loseString = `You lose! ${computerChoice} beats ${humanChoice}`;
+    function playRound(humanChoice, computerChoice) {
+        const drawString = `Draw! ${humanChoice} matches ${computerChoice}`;
+        const winString = `You win! ${humanChoice} beats ${computerChoice}`;
+        const loseString = `You lose! ${computerChoice} beats ${humanChoice}`;
 
-    if (humanChoice == computerChoice) {
-        console.log(drawString);
-        return;
-    }
+        if (humanChoice == computerChoice) {
+            console.log(drawString);
+            return;
+        }
 
-    if (humanChoice == 'rock') {
-        if (computerChoice == 'paper') {
-            console.log(loseString);
-            computerScore++;
-        } else {
-            console.log(winString);
-            humanScore++;
+        if (humanChoice == 'rock') {
+            if (computerChoice == 'paper') {
+                console.log(loseString);
+                computerScore++;
+            } else {
+                console.log(winString);
+                humanScore++;
+            }
+        }
+
+        if (humanChoice == 'paper') {
+            if (computerChoice == 'scissors') {
+                console.log(loseString);
+                computerScore++;
+            } else {
+                console.log(winString);
+                humanScore++;
+            }
+        }
+
+        if (humanChoice == 'scissors') {
+            if (computerChoice == 'rock') {
+                console.log(loseString);
+                computerScore++;
+            } else {
+                console.log(winString);
+                humanScore++;
+            }
         }
     }
 
-    if (humanChoice == 'paper') {
-        if (computerChoice == 'scissors') {
-            console.log(loseString);
-            computerScore++;
-        } else {
-            console.log(winString);
-            humanScore++;
-        }
+    for (let i = 0; i < 5; i++) {
+        console.log(`Your score: ${humanScore}`);
+        console.log(`Computer score: ${computerScore}`);
+
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
     }
 
-    if (humanChoice == 'scissors') {
-        if (computerChoice == 'rock') {
-            console.log(loseString);
-            computerScore++;
-        } else {
-            console.log(winString);
-            humanScore++;
-        }
+    if (humanScore > computerScore) {
+        console.log('You win the game!');
+    } else if (computerScore > humanScore) {
+        console.log('You lose the game.');
+    } else {
+        console.log('Draw.');
     }
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
+playGame();
